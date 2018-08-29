a9e8046a9b14625a452426211e8a91a0a31bd6c8
T0T0R <arthur.electron@orange.fr>
2018-08-29 17:48:07 +0200

    minor error correction

--- include/Vect3D.h
+++ include/Vect3D.h
@@ -10,23 +10,23 @@
 class Vect3D
 {
 public:
-	Vect3D::Vect3D();
-	Vect3D::Vect3D(double X, double Y, double Z);
+	Vect3D();
+	Vect3D(double X, double Y, double Z);
 
-	double Vect3D::getX() const;
-	double Vect3D::getY() const;
-	double Vect3D::getZ() const;
+	double getX() const;
+	double getY() const;
+	double getZ() const;
 
-	int Vect3D::setX(double X);
-	int Vect3D::setY(double Y);
-	int Vect3D::setZ(double Z);
+	int setX(double X);
+	int setY(double Y);
+	int setZ(double Z);
 
-	int Vect3D::debug() const ;
-	std::string Vect3D::datas() const;
+	int debug() const ;
+	std::string datas() const;
 
 
-	bool Vect3D::operator==(const Vect3D& vect);
-	bool Vect3D::operator!=(const Vect3D& vect);
+	bool operator==(const Vect3D& vect);
+	bool operator!=(const Vect3D& vect);
 
 protected:
 
